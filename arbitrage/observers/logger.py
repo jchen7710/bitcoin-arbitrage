6e95705356237238cfad1d3fa3a37f68df27291a
jinghui mo <jinghui mo>
2017-06-24 22:26:51 -0400

    write log to file

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -1,6 +1,17 @@
 import logging
 from .observer import Observer
 
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+logPath='log'
+fileName='profit'
+fileHandler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
 
 class Logger(Observer):
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
